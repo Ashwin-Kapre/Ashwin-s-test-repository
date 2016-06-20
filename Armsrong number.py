4799590659b347282b3c7e9b103f2ffec913e6c4
Ashwin-Kapre <ashwinpksatara@gmail.com>
2016-06-20 15:31:29 +0000

    Add files via upload

---
+++ Armsrong number.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 05 09:29:11 2015
+
+@author: 15BCE0713
+"""
+
+a=int(input("Enter a value"))
+x=0
+while(a>0):
+    a=a//10
+    x=x+1
+    print(x)
+    
+    
